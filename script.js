3438347b8cae88e62e4b9567b66c5d30ab8b3de7
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-12-05 17:59:23 +0000

    add functional remove button to the book card and preventDefault() in form

--- script.js
+++ script.js
@@ -1,42 +1,65 @@
 const myLibrary = [];
 const modal = document.querySelector(".book-details");
 const display = document.querySelector(".lib-container");
-const addBookbtn = document.querySelector(".new-book");
+const addBookbtn = document.querySelector(".add-card");
 const closebtn = document.querySelector(".close-modal");
 const submitbtn = document.querySelector(".submit-book");
+const bookForm = document.getElementById("book-entry");
+
+bookForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent default form submission
+    const formData = new FormData(bookForm);
+    const title = formData.get("book-title");
+    const author = formData.get("author");
+    const readingstatus = formData.get("status");
+    const book = new Book(title, author, readingstatus);
+    myLibrary.push(book);
+    addBookToLibrary(book);
+  });
+
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('delete-btn')) {
+    const parent = event.target.closest('.book');
+    if (parent) {
+      parent.remove();
+    }
+  }
+});
 
 addBookbtn.addEventListener('click', enterDetails);
-submitbtn.addEventListener('click', addBookToLibrary);
 closebtn.addEventListener('click', () => modal.close(undefined));
 
 function Book(title, author, readingstatus) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
-    this.title = title;
-    this.author = author;
-    this.readingstatus = readingstatus;
+    this.Title = title;
+    this.Author = author;
+    this.Status = readingstatus;
+    this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(){
-    const title = document.querySelector("#book-title").value;
-    const author = document.querySelector("#author").value;
-    const readingstatus = document.querySelector("#status").value;
-    const book = new Book(title, author, readingstatus);
-    myLibrary.push(book);
+function addBookToLibrary(book){
     let card = document.createElement("div");
-    card.classList.add("card");
+    card.classList.add("book");
     for(const key in book){
+        if(key == 'id')   continue;
         let info = document.createElement("p");
         info.innerText += `${key} : ${book[key]}`;
         card.append(info);
     }
+    let delbtn = document.createElement("button");
+    delbtn.innerText = "Remove";
+    delbtn.classList.add("delete-btn");
+    card.append(delbtn);
     display.append(card);
 }
 
-function removeBook(){
-
-}
+// function removeBook(id){
+//     const idx = myLibrary.findIndex(book => book.id === id);
+//     myLibrary.splice(idx, 1);
+// }
 
 function enterDetails(){
     modal.showModal();
